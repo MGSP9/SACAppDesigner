99ea81d10634c403b4137f2906184ea8de13a3f8
MGSP9 <92260977+MGSP9@users.noreply.github.com>
2024-08-11 20:58:24 +0200

    Update PerfomanceToolLogic.js

--- PerfomanceToolLogic.js
+++ PerfomanceToolLogic.js
@@ -151,7 +151,11 @@ function processDuplicates(text, keyword) {
             console.log ("New text");
             console.log (`${keyword.slice(1)}_${match.split('.')[0]}`);
                       
-             newText = newText.replace(new RegExp(match+"()", 'g'), `${keyword.slice(1)}_${match.split('.')[0]}`);
+            newText = newText.replace(new RegExp(match+"()", 'g'), `${keyword.slice(1)}_${match.split('.')[0]}`);
+                        
+            console.log (newText);
+            newText = newText.replace (`${keyword.slice(1)}_${match.split('.')[0]}`+"()", `${keyword.slice(1)}_${match.split('.')[0]}`);
+            
             console.log (newText);
             newText = `${variableName}\n${newText}`;
               console.log (newText);
