02dcce30eadf89e9dd69fad2a599dc56c1569152
MGSP9 <92260977+MGSP9@users.noreply.github.com>
2024-08-11 17:33:53 +0200

    Update PerfomanceToolLogic.js

--- PerfomanceToolLogic.js
+++ PerfomanceToolLogic.js
@@ -117,7 +117,14 @@ function showResults() {
     `;
 
     // Scroll to the results section
-    document.getElementById('optimization').scrollIntoView({ behavior: 'smooth' });
+
+    // Set the progress-list element above the optimization document
+    const progressList = document.querySelector('.progress-list');
+    const optimizationSection = document.getElementById('optimization');
+    optimizationSection.parentNode.insertBefore(progressList, optimizationSection);
+}
+    
+    document.getElementById('.progress-list').scrollIntoView({ behavior: 'smooth' });
 }
 
 function getInTouch() {
